9b20cc2ccea8529f87f1040e6c2c2b3dc2999ff6
Jaysri07 <jayasrikannan93@gmail.com>
2022-04-03 18:20:17 +0530

    Changes done in new branch

--- src/org/test/git/Demo.java
+++ src/org/test/git/Demo.java
@@ -2,8 +2,8 @@ package org.test.git;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Program no : #1");
-		System.out.println("Program name: Demo program");
-		System.out.println("New branch 'Jayasri' is added");
+		System.out.println("Program no : #1 ");
+		System.out.println("Program name: Demo program ");
+		System.out.println("New branch 'Jayasri' is added ");
 	}
 }
